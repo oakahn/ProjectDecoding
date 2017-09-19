3cb778a002cfb6d2af2be5cadac6ca0a1636252e
Todsphol <theaddza11@gmail.com>
2017-09-19 16:03:03 +0700

    Design UI Decoding

--- app/src/main/java/add/wongchomphu/co/th/decoing/MainFragment.kt
+++ app/src/main/java/add/wongchomphu/co/th/decoing/MainFragment.kt
@@ -13,7 +13,6 @@ class MainFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-
         val view = inflater!!.inflate(R.layout.fragment_mian, container, false)
         ButterKnife.bind(this, view)
         return view
