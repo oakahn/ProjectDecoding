7dfefa14f3189c7cc4fc6f6d25cba512f6d62e68
Todsphol <theaddza11@gmail.com>
2017-09-19 16:35:02 +0700

    Create array A-Z

--- app/src/main/java/add/wongchomphu/co/th/decoing/MainFragment.kt
+++ app/src/main/java/add/wongchomphu/co/th/decoing/MainFragment.kt
@@ -6,16 +6,27 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import butterknife.BindView
 import butterknife.ButterKnife
 
 class MainFragment : Fragment() {
-
-
+    @BindView(R.id.tv_show_decoding) lateinit var tvShowCode : TextView
+    @BindView(R.id.edt_encode) lateinit var  edtEncode : EditText
+    @BindView(R.id.edt_input_k) lateinit var edtValueK : EditText
+    @BindView(R.id.btn_decode) lateinit var btnDecode : Button
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view = inflater!!.inflate(R.layout.fragment_mian, container, false)
         ButterKnife.bind(this, view)
+        Encode()
         return view
+    }
+    fun Encode() {
+        val a = listOf('A'..'Z').flatten()
+        tvShowCode.text = a[25].toString()
     }
     companion object {
         fun newInstance() : MainFragment {
