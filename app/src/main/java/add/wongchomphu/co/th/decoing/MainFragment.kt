e9c2635a913922d48dc1fc42d83fb96b1a452509
Todsphol <theaddza11@gmail.com>
2017-09-20 16:46:06 +0700

    fucntion Shif Ceasar is correct but app have error cuz not input key

--- app/src/main/java/add/wongchomphu/co/th/decoing/MainFragment.kt
+++ app/src/main/java/add/wongchomphu/co/th/decoing/MainFragment.kt
@@ -13,23 +13,49 @@ import butterknife.BindView
 import butterknife.ButterKnife
 
 class MainFragment : Fragment() {
-    @BindView(R.id.tv_show_decoding) lateinit var tvShowCode : TextView
-    @BindView(R.id.edt_encode) lateinit var  edtEncode : EditText
-    @BindView(R.id.edt_input_k) lateinit var edtValueK : EditText
-    @BindView(R.id.btn_decode) lateinit var btnDecode : Button
+    @BindView(R.id.tv_show_decoding) lateinit var tvShowCode: TextView
+    @BindView(R.id.edt_encode) lateinit var edtEncode: EditText
+    @BindView(R.id.edt_input_k) lateinit var edtValueK: EditText
+    @BindView(R.id.btn_decode) lateinit var btnDecode: Button
+    @BindView(R.id.tv_show_encrypt) lateinit var tvShowEncrypt : TextView
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view = inflater!!.inflate(R.layout.fragment_mian, container, false)
         ButterKnife.bind(this, view)
-        Encode()
+
+        btnDecode.setOnClickListener {
+        val key = edtValueK.text.toString()
+        val encoded = encrypt(edtEncode.text.toString(),key.toInt())
+            tvShowEncrypt.text = encoded
+            tvShowCode.text = decrypt(encoded,key = key.toInt())
+        }
         return view
     }
-    fun Encode() {
-        val a = listOf('A'..'Z').flatten()
-        tvShowCode.text = a[25].toString()
+    fun encrypt(s: String, key: Int): String {
+        val offset = key % 26
+        if (offset == 0) return s
+        var d: Char
+        val chars = CharArray(s.length)
+        for ((index, c) in s.withIndex()) {
+            if (c in 'A'..'Z') {
+                d = c + offset
+                if (d > 'Z') d -= 26
+            }
+            else if (c in 'a'..'z') {
+                d = c + offset
+                if (d > 'z') d -= 26
+            }
+            else
+                d = c
+            chars[index] = d
+        }
+        return chars.joinToString("")
     }
+
+    fun decrypt(s: String, key: Int): String = encrypt(s, 26 - key)
+
     companion object {
-        fun newInstance() : MainFragment {
+        fun newInstance(): MainFragment {
             val bundle = Bundle()
             val fragment = MainFragment()
             fragment.arguments = bundle
